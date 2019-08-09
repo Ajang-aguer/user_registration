0f40a3b2680bdd2c1d0b78bb885ab35b859abb09
Ajang-aguer <mjmonyjang@gmail.com>
2019-08-09 16:59:50 -0600

    First Commit

---
+++ account/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+    RegexValidator,
+)
+from django.db import models
+
+  
---
+++ userRegenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ajang\desktop\projects\python\django\user_registration\userregenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
